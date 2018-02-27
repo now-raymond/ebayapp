7de43b7c9d25fa485b8199456cdc3cfe1254a3fd
Jie Xen In <jasonin@Jies-MacBook-Air.local>
2018-02-27 01:53:22 +0000

    Implement Bootstrap 4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,31 +6,78 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Input;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request as GuzzleRequest;
+
+
 class ProductController extends Controller {
 
     // Search 'products' table for any relavent products using $productName 
     public function search(Request $request){
         $productName = $request->input('query');
-        // return: id, ebay_id, name, last_known_price
-        $products = DB::select("SELECT * FROM products WHERE name LIKE ?", array('%' . $productName . '%'));
+        
+        // $token = $request->session()->get('token', 'not found');
+        // if($token == 'not found'){
+        //     $token = $this->getToken();
+        //     $request->session()->put('token', $token);
+        // }
 
+        $response = $this->searchItem('', $productName);
+
+        if($response->total > 0){
+            // Insert new record to 'products' table if has not been searched perviously
+            $items = $response->itemSummaries;
+            foreach ($items as $item) {
+                $isProductExist = DB::select("SELECT last_known_price FROM products WHERE ebay_id = ?", array($item->itemId));
+                if($isProductExist == null){
+
+                    DB::insert("INSERT INTO products (ebay_id, name, image, last_known_price) VALUES (?, ?, ?, ?)", array($item->itemId, $item->title, $item->image->imageUrl, $item->price->value));   
+                    $record = DB::select("SELECT id FROM products WHERE ebay_id = ?", array($item->itemId));
+
+                    // TODO: Input correct timestamp 
+                    DB::insert("INSERT INTO price_history (product_id, price) VALUES (?, ?)", array($record[0]->id, $item->price->value));   
+                }else{
+                    // Update the last_known_price if the product exists
+                    DB::update("UPDATE products SET last_known_price = ? WHERE ebay_id = ?", array($item->price->value, $item->itemId));
+                }   
+            }
+        }else{
+            $items = null;
+        }
+       
         return view('search', [
-            'products' => $products,
+            'products' => $items,
         ]);
     }
 
     // Search 'price_history' table for selected product record(s) using $productId 
     public function item($id){
         // return: product_id, price, timestamp (JOIN 'products' table to for name)
-        $query = "SELECT products.name, price_history.product_id, price_history.price, price_history.timestamp 
-                    FROM products 
-                    JOIN price_history ON price_history.product_id = products.id
-                    WHERE products.id = ?
+        $query = "SELECT price_history.product_id, price_history.price, price_history.timestamp 
+                    FROM products JOIN price_history 
+                    ON price_history.product_id = products.id
+                    WHERE products.ebay_id = ?
                     ORDER BY price_history.timestamp DESC";
+
+        // Product price history
         $records = DB::select($query, array($id));
+        // Product latest information 
+        $response = $this->getItem($id);
+
+        // Check if $response->shortDescription property is empty
+        $shortDescription = '';
+        if(property_exists($response, 'shortDescription')){
+            $shortDescription = $response->shortDescription;
+        }
+
+        $product = (object)['title'=>$response->title, 
+                            'details'=>$shortDescription,
+                            'price'=>$response->price->value,
+                            'image'=>$response->image->imageUrl];
 
         return view('item', [
             'records' => $records,
+            'product' => $product,
         ]);
     }
 
@@ -60,7 +107,7 @@ class ProductController extends Controller {
         }
 
         // Retrieve products watched by the user
-        $query = "SELECT products.id, products.name, products.last_known_price
+        $query = "SELECT *
                     FROM products 
                     JOIN product_watches ON products.id = product_watches.product_id
                     WHERE product_watches.user_id = ?";
@@ -83,10 +130,11 @@ class ProductController extends Controller {
         }
 
         // Retrieve products watched by the user
-        $query = "SELECT products.id, products.name, products.last_known_price
+        $query = "SELECT *
                     FROM products 
                     JOIN product_watches ON products.id = product_watches.product_id
                     WHERE product_watches.user_id = ?";
+
         $watching = DB::select($query, array($user[0]->id));
         
         return view('watchlist', [
@@ -94,4 +142,48 @@ class ProductController extends Controller {
         ]);        
     }
 
+
+
+    // TODO: Fix scope issue
+    private function getToken(){
+        $client = new Client(['base_uri' => 'https://api.ebay.com']);
+
+        $authorization = 'Basic ' . base64_encode(env('EBAY_APP_ID') . ':' . env('EBAY_CERT_ID')); 
+        $headers = ['Content-Type' => 'application/x-www-form-urlencoded', 'Authorization' => $authorization];
+        $body = 'grant_type=client_credentials&redirect_uri=' . env('EBAY_RU_NAME') . '&scope=https://api.ebay.com/oauth/api_scope';
+
+        $request = new GuzzleRequest('POST', '/identity/v1/oauth2/token', $headers, $body);
+        $response = $client->send($request, ['timeout' => 2]);
+        $reponseBody = json_decode($response->getBody(), true);
+
+        $token = $reponseBody['access_token'];
+        return $token;
+    }
+
+    // TODO: If above is fixed, this will also be fixed
+    private function searchItem($token, $keyword){
+        $client = new Client(['base_uri' => 'https://api.ebay.com']);
+    
+        $authorization = 'Bearer v^1.1#i^1#r^0#f^0#p^1#I^3#t^H4sIAAAAAAAAAOVXbWwURRju9gsrUBARDJJwLMWIuLuzu/e5cGeOK02P0PbkaqmcpNnbnW0X9nY3O3OUgx/UmhCjvzAaQkKA4EcgIgGBxIghTTQYMAS/QGNQEmMTQYkYAiYaP2b3jnKtBChUIfH+XOadd955n+d93pkd0Fdb9/jG5o2/TqTGVe7oA32VFMWPB3W1NfPrqypn1FSAMgdqR19DX3V/1Q8LkZwzbGkZRLZlIuhbmzNMJHnGKJ13TMmSkY4kU85BJGFFSsdblkoCCyTbsbClWAbtSzZG6YgW4EWZF0KaIAeyKiRW82rMditKK3xIA0GohYNBNRKJKGQeoTxMmgjLJo7SAuDDDBAYIdQOgMT7JT/PhsXwCtrXAR2kWyZxYQEd89KVvLVOWa43TlVGCDqYBKFjyXhTui2ebFzc2r6QK4sVK/GQxjLOo+GjhKVCX4ds5OGNt0Get5TOKwpEiOZixR2GB5XiV5O5jfQ9qkVV5AOqXwjxETEShKExobLJcnIyvnEerkVXGc1zlaCJdVy4GaOEjewqqODSqJWESDb63L+n8rKhazp0ovTiRfFn4qkUHWux1vWosCfOIMWRbegoTGpZI+MPqGGgilmVCYRASBRUtbRRMVqJ5hE7JSxT1V3SkK/VwosgyRqO5AaUcUOc2sw2J65hN6NyP+Eqh4K4wi1qsYp53GO6dYU5QoTPG968AkOrMXb0bB7DoQgjJzyKorRs27pKj5z0tFiSz1oUpXswtiWO6+3tZXtF1nK6OQEAnutsWZpWemBOpomv2+tFf/3mCxjdg6KQNib+Ei7YJJe1RKskAbObjhHtBUPBEu/D04qNtP7DUIaZG94RY9UhfID0BlBgRAwHFEXhx6JDYiWRcm4eMCsXmJzsrIbYNmQFMgrRWT4HHV2VxIAmiGENMmowojH+iKYx2YAaZHgNQgBhNqtEwv+nRrlVqacVy4Ypy9CVwpgIfszELjpqSnZwIQ0NgxhuVfXXBYlckP86PLfXRwXRjYFIENnWWVfbrGLlOEsmh5pr6vKyviPcOrkP76miEoBFpLpavMhYDy6L1iisA5GVd8gdzra553q7tRqapEuwYxkGdDr4O2Ji7E70u3SaXxeVYuiExq57Ddkoj8nb1LaM7yLq6n4qcx3kfICPBCPAH74ztSa8urYX/oNDa1SFbbYQhrdctlF8gHDDn0OxCu/H91OHQD+1n7yoAAfm8nPA7Nqqp6urJsxAOoasLmss0rtN8pXvQHY1LNiy7lTWUpmZ+3Z3lT3AdqwEDw89weqq+PFl7zEw89pMDT9p+kQ+DAQhRFjx+/kVYM612Wp+WvXUM6v+gC25qguvn7lw5ZMNXCIz8PbXYOKQE0XVVBBlVDQv+NbeErl0NtOrJ6bMb9i89dw6Tuk8nHjgWGvHmXF7D1ZsGdxjrcp+eTl+/juHvsw/Vz/YebjjcKLQenRl4sUDu79AGfGJRzfJ2658/Fa9nTm5dbO1/1gCv5s8MXXJzqBxWn1hUvx4z/K6Cd8fP3Fi7qntr31EzfylbiB2X/6bNw8+0hBE9M/TJp8P/pkZFz2wbUq7+cG+eZ2fH2pakGp+ef9jT743feeSi+xf087NeyjXN9ggNpyD9qYj7+Dug13r7z9wqRB9dmD5LG4DNSt18lTLnsHfjc5P699XP0sePfL8Wf2rV9dw0xed/vDUby+df7Cx8idqlzOwsyJT++PFyXtfWbBk/frqN2bvKpbvb1cj8ygaDwAA';
+        $headers = ['Authorization' => $authorization];
+
+        $request = new GuzzleRequest('GET', '/buy/browse/v1/item_summary/search?q=' . $keyword . '&limit=12', $headers);
+        $response = $client->send($request, ['timeout' => 5]);
+        $reponseBody = json_decode($response->getBody());
+
+        return $reponseBody;
+    }
+
+    private function getItem($id){
+        $client = new Client(['base_uri' => 'https://api.ebay.com']);
+
+        $authorization = 'Bearer v^1.1#i^1#r^0#f^0#p^1#I^3#t^H4sIAAAAAAAAAOVXbWwURRju9gsrUBARDJJwLMWIuLuzu/e5cGeOK02P0PbkaqmcpNnbnW0X9nY3O3OUgx/UmhCjvzAaQkKA4EcgIgGBxIghTTQYMAS/QGNQEmMTQYkYAiYaP2b3jnKtBChUIfH+XOadd955n+d93pkd0Fdb9/jG5o2/TqTGVe7oA32VFMWPB3W1NfPrqypn1FSAMgdqR19DX3V/1Q8LkZwzbGkZRLZlIuhbmzNMJHnGKJ13TMmSkY4kU85BJGFFSsdblkoCCyTbsbClWAbtSzZG6YgW4EWZF0KaIAeyKiRW82rMditKK3xIA0GohYNBNRKJKGQeoTxMmgjLJo7SAuDDDBAYIdQOgMT7JT/PhsXwCtrXAR2kWyZxYQEd89KVvLVOWa43TlVGCDqYBKFjyXhTui2ebFzc2r6QK4sVK/GQxjLOo+GjhKVCX4ds5OGNt0Get5TOKwpEiOZixR2GB5XiV5O5jfQ9qkVV5AOqXwjxETEShKExobLJcnIyvnEerkVXGc1zlaCJdVy4GaOEjewqqODSqJWESDb63L+n8rKhazp0ovTiRfFn4qkUHWux1vWosCfOIMWRbegoTGpZI+MPqGGgilmVCYRASBRUtbRRMVqJ5hE7JSxT1V3SkK/VwosgyRqO5AaUcUOc2sw2J65hN6NyP+Eqh4K4wi1qsYp53GO6dYU5QoTPG968AkOrMXb0bB7DoQgjJzyKorRs27pKj5z0tFiSz1oUpXswtiWO6+3tZXtF1nK6OQEAnutsWZpWemBOpomv2+tFf/3mCxjdg6KQNib+Ei7YJJe1RKskAbObjhHtBUPBEu/D04qNtP7DUIaZG94RY9UhfID0BlBgRAwHFEXhx6JDYiWRcm4eMCsXmJzsrIbYNmQFMgrRWT4HHV2VxIAmiGENMmowojH+iKYx2YAaZHgNQgBhNqtEwv+nRrlVqacVy4Ypy9CVwpgIfszELjpqSnZwIQ0NgxhuVfXXBYlckP86PLfXRwXRjYFIENnWWVfbrGLlOEsmh5pr6vKyviPcOrkP76miEoBFpLpavMhYDy6L1iisA5GVd8gdzra553q7tRqapEuwYxkGdDr4O2Ji7E70u3SaXxeVYuiExq57Ddkoj8nb1LaM7yLq6n4qcx3kfICPBCPAH74ztSa8urYX/oNDa1SFbbYQhrdctlF8gHDDn0OxCu/H91OHQD+1n7yoAAfm8nPA7Nqqp6urJsxAOoasLmss0rtN8pXvQHY1LNiy7lTWUpmZ+3Z3lT3AdqwEDw89weqq+PFl7zEw89pMDT9p+kQ+DAQhRFjx+/kVYM612Wp+WvXUM6v+gC25qguvn7lw5ZMNXCIz8PbXYOKQE0XVVBBlVDQv+NbeErl0NtOrJ6bMb9i89dw6Tuk8nHjgWGvHmXF7D1ZsGdxjrcp+eTl+/juHvsw/Vz/YebjjcKLQenRl4sUDu79AGfGJRzfJ2658/Fa9nTm5dbO1/1gCv5s8MXXJzqBxWn1hUvx4z/K6Cd8fP3Fi7qntr31EzfylbiB2X/6bNw8+0hBE9M/TJp8P/pkZFz2wbUq7+cG+eZ2fH2pakGp+ef9jT743feeSi+xf087NeyjXN9ggNpyD9qYj7+Dug13r7z9wqRB9dmD5LG4DNSt18lTLnsHfjc5P699XP0sePfL8Wf2rV9dw0xed/vDUby+df7Cx8idqlzOwsyJT++PFyXtfWbBk/frqN2bvKpbvb1cj8ygaDwAA';
+        $headers = ['Authorization' => $authorization];
+       
+        $request = new GuzzleRequest('GET', '/buy/browse/v1/item/' . $id , $headers);
+        $response = $client->send($request, ['timeout' => 5]);
+        $reponseBody = json_decode($response->getBody()); 
+
+        return $reponseBody;
+    }
 }
--- resources/views/watchlist.blade.php
+++ resources/views/watchlist.blade.php
@@ -6,23 +6,117 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>EBAE</title>
+        <!-- Bootstrap -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+        <!-- Font -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <!-- <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}" type="text/css">  -->
+
+
+        <style>
+            .hideOverflow {
+                overflow:hidden;
+                white-space:nowrap;
+                text-overflow:ellipsis;
+                width:100%;
+                display:block;
+            }
+
+            /* The following overwrite some bootstrap css properties */
+            .card{
+                margin: 0 0 20px 0;
+                padding: 20px;
+            }
+
+            .card-img-top {
+                width: 100%;
+                height: 15vw;
+                object-fit: cover;
+            }       
+            
+            .card-title{
+                padding-top:10px;
+            }
+        </style>
+
     </head>
 
     <body>
-        <h1>Watch list</h1>
-        @if(count($products) > 0)
-            @foreach ($products as $product)	
-                <div>
-                    <!--Image-->
-                    <div></div>
-                    <p>ProductID - {{ $product->id }}</p>
-                    <p>Product name - {{ $product->name }}</p>
-                    <p>Price - {{ $product->last_known_price }}</p>
+        <!-- Navigation bar -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
+            <a class="navbar-brand" href="/">HOME</a>
+
+            <!-- Search bar -->
+            <form id="searchForm" class="form-inline" method="GET" action="/search">
+                <input id="searchInput" class="form-control mr-sm-2" style="width:300px" type="text" name="query" placeholder="Search for an item here" aria-label="Search" required>
+            </form>       
+            
+            <button type="button" class="btn btn-outline-success" data-toggle="modal" data-target="#emailModal">My watch list</button>
+        </nav>
+
+
+        <!-- Products found -->
+        <div class="container" style="padding:30px 0 70px 0">
+            <div class="card-group row">
+                @if($products != null)
+                    @foreach ($products as $product)
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3" style="text-decoration: none;">	
+                        <div class="card link-no-style">
+                            <a href="/item/{{ $product->ebay_id }}"><img class="card-img-top" src="{{ $product->image }}"></a>
+                            <div class="card-block">
+                                <h5 class="card-title hideOverflow">{{ $product->name }}</h5>
+                                USD {{ $product->last_known_price }}
+                            </div>
+                        </div>      
+                     </div>
+                    @endforeach
+                @endif     
+            </div>           
+        </div>
+
+
+       <!-- Modal -->
+        <div class="modal" id="emailModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Tell us your email address...</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <form id="watchListForm" method="GET" action="/watchlist">
+                        <div class="modal-body">               
+                            <input id="emailInput" class="form-control mr-sm-2" type="email" name="email" placeholder="EG. abc@xyz.com" aria-label="Search" required>
+                        </div>
+
+                        <div class="modal-footer">
+                            <input id="confirmEmailBtn" type="submit" class="btn btn-success" value="Confirm"></input>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </form>                               
                 </div>
-                <hr>
-            @endforeach
-        @endif
-        <a href="/">Back to home page</a>
+            </div>
+        </div>
     </body>
+
+
+    <script type="text/javascript">
+        $('#emailModal').on('shown.bs.modal', function () {
+            $('#emailInput').trigger('focus')
+        })  
+
+        $("#searchInput").keypress(function(e){
+            if (e.keyCode == 13){
+                if($("#searchInput").val()){
+                    $('#searchForm').submit();
+                }
+            }
+        });    
+    </script>
 </html>
